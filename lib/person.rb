3aa6fdced66afc023d9cf9e7d2ac8681077e9aa1
Jonathan Luk <jluk87@gmail.com>
2018-01-19 04:24:26 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
   def initialize(person_hash)
-    person_hash.each {|attribute, data| self.send(("#{attribute=}"),data)}
+    person_hash.each {|attribute, data| self.send(("#{attribute=}"), data)}
   end
 end
